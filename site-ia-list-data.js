f958e349d414275baa903255e59d960f2dc300f4
Tempo Inteligente.com <tempo.inteligente.ia@gmail.com>
2026-07-23 01:47:13 -0300

    Atualiza lista: IA no Cotidiano

--- site-ia-list-data.js
+++ site-ia-list-data.js
@@ -2,11 +2,11 @@
 const IA_LIST = [
   {
     "id": "iamrx0wi2x6y1bi",
-    "titulo": "",
-    "resumo": "",
+    "titulo": "Chat GPT",
+    "resumo": "Assistente de IA versátil que ajuda a responder perguntas, criar textos, resumir conteúdos, estudar, programar e aumentar a produtividade no dia a dia.",
     "logoUrl": "",
-    "videoUrl": "",
-    "novidade": false,
-    "novidadeDefinida": false
+    "videoUrl": "videos/iamrx0wi2x6y1bi.mp4",
+    "novidade": true,
+    "novidadeDefinida": true
   }
 ];
