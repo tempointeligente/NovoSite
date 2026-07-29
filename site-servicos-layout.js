0ee081620cdb3764b5dce0b10fb47d65f41985d2
Tempo Inteligente.com <tempo.inteligente.ia@gmail.com>
2026-07-29 05:57:55 -0300

    Atualiza layout: Serviços

---
+++ site-servicos-layout.js
@@ -0,0 +1,41 @@
+/* Layout da página Serviços. Gerado pelo Painel Administrativo. */
+const SERVICOS_LAYOUT = {
+  "titulo": "Serviços",
+  "subtitulo": "Conheça algumas ferramentas do Tempo Inteligente, que podem ajudar no seu dia dia",
+  "cards": {
+    "guias": {
+      "cardTop": 23.92,
+      "cardLeft": 5.05,
+      "cardWidth": 40,
+      "cardHeight": 15.17
+    },
+    "ia": {
+      "cardTop": 23.92,
+      "cardLeft": 56.47,
+      "cardWidth": 40,
+      "cardHeight": 15.17
+    },
+    "produtos": {
+      "cardTop": 50.02,
+      "cardLeft": 5.05,
+      "cardWidth": 40,
+      "cardHeight": 15.15
+    },
+    "produtividade": {
+      "cardTop": 50.02,
+      "cardLeft": 56.47,
+      "cardWidth": 40,
+      "cardHeight": 15.15
+    }
+  },
+  "tituloLayout": {
+    "top": 4.3246846775211685,
+    "left": 0,
+    "width": 40
+  },
+  "subtituloLayout": {
+    "top": 10.937286172543455,
+    "left": 12.782251392685179,
+    "width": 70
+  }
+};
